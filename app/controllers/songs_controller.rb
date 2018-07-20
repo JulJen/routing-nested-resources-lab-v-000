3e7e51a30d3baac0b0ac2ddce3aff9f897c32039
Jul Jen <juliajen.prog@gmail.com>
2018-07-20 20:44:28 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,80 +1,28 @@
 class SongsController < ApplicationController
 
-
   def index
-  if params[:artist_id]
-    @artist = Artist.find_by(id: params[:artist_id])
-    if @artist.nil?
-      redirect_to artists_path, alert: "Artist not found"
+    if params[:artist_id]
+      @artist = Artist.find_by(id: params[:artist_id])
+      if @artist.nil?
+        # flash[:alert] = "Artist not found."
+        redirect_to artists_path
+      else
+        # @songs = Song.where("artist_id=?", params[:artist_id])
+        @songs = @artist.songs
+      end
     else
-      @songs = @artist.songs
+      @songs = Song.all
     end
-  else
-    @songs = Song.all
   end
-end
 
-def show
-  if params[:artist_id]
-    @artist = Artist.find_by(id: params[:artist_id])
-    @song = @artist.songs.find_by(id: params[:id])
-    if @song.nil?
-      redirect_to artist_songs_path(@artist), alert: "Song not found"
+  def show
+    if params[:artist_id] && Song.find_by(id: params[:id]).nil?
+      flash[:alert] = "Song not found."
+      redirect_to artist_songs_path(params[:artist_id])
+    else
+      @song = Song.find(params[:id])
     end
-  else
-    @song = Song.find(params[:id])
   end
-end
-
-
-#   # it "redirects to artists songs when artist song not found" do
-#   #   get :show, id: 12345, artist_id: @artist.id
-#   #   expect(controller).to set_flash[:alert]
-#   #   expect(response).to redirect_to artist_songs_path(@artist)
-#   # end
-#
-#   def index
-#     if params[:artist_id]
-#       @artist = Artist.find_by(id: params[:artist_id])
-#       if @artist.nil?
-#         # flash[:alert] = "Artist not found."
-#         redirect_to artists_path
-#       else
-#         # @songs = Song.where("artist_id=?", params[:artist_id])
-#         @songs = @artist.songs
-#       end
-#     else
-#       @songs = Song.all
-#     end
-#   end
-#
-#
-# #   def show
-# #     if params[:artist_id]
-# #       @artist = Artist.find_by(id: params[:artist_id])
-# #       @song = @artist.songs.find_by(id: params[:id])
-# # # binding.pry
-# #       # if @song.nil?
-# #       # if Song.find_by(id: params[:id]).nil?
-# #       #   flash[:alert] = "Song not found."
-# #       #   redirect_to artist_songs_path(params[:artist_id])
-# #       if params[:artist_id] && Song.find_by(id: params[:id]).nil?
-# #         flash[:alert] = "Song not found."
-# #         redirect_to artist_songs_path(params[:artist_id])
-# #       end
-# #     else
-# #       @song = Song.find(params[:id])
-# #     end
-# #   end
-#
-#   def show
-#     if params[:artist_id] && Song.find_by(id: params[:id]).nil?
-#       flash[:alert] = "Song not found."
-#       redirect_to artist_songs_path(params[:artist_id])
-#     else
-#       @song = Song.find(params[:id])
-#     end
-#   end
 
 
   def new
